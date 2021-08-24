1479298ea504d5b82f569d27d1a6c0be2adec4a0
Maurice Marteau <55093229+maumarteau@users.noreply.github.com>
2021-08-24 14:53:49 -0300

    'deploy'

--- web/src/App.vue
+++ web/src/App.vue
@@ -12,21 +12,19 @@ export default {
 		titleTemplate: '%s | Lestido Renting',
 	},
 	beforeMount() {
-		let isLocalhost = false
-		let redir = false
-
-		const url = 'https://www.renting.com.uy' + window.location.pathname
-		if (window.location.origin.indexOf('localhost') >= 0) {
-			isLocalhost = true
-		}
-		if (window.location.origin.indexOf('www') == -1) {
-			redir = true
-		}
-
-		if (window.location.origin.indexOf('http:') == 0) {
-			redir = true
-		}
-		if (!isLocalhost && redir) window.location = url
+		// let isLocalhost = false
+		// let redir = false
+		// const url = 'https://www.renting.com.uy' + window.location.pathname
+		// if (window.location.origin.indexOf('localhost') >= 0) {
+		// 	isLocalhost = true
+		// }
+		// if (window.location.origin.indexOf('www') == -1) {
+		// 	redir = true
+		// }
+		// if (window.location.origin.indexOf('http:') == 0) {
+		// 	redir = true
+		// }
+		// if (!isLocalhost && redir) window.location = url
 	},
 }
 </script>
