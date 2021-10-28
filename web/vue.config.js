edb77c2698e77098fa39384e063436e344b37e33
Maurice Marteau <55093229+maumarteau@users.noreply.github.com>
2021-10-28 11:55:51 -0300

    folder web

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/web/' : '/',
+	publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 
 	// pwa: {
 	// 	name: 'Doctor Consultas',
