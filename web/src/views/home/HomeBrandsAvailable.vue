a9a4ef3b0a0bf670649d26eef3246e60a23c8b37
Maurice Marteau <maurice@play.com.uy>
2023-08-08 17:19:45 -0300

    deploy

--- web/src/views/home/HomeBrandsAvailable.vue
+++ web/src/views/home/HomeBrandsAvailable.vue
@@ -2,9 +2,7 @@
 	<div class=" mt-5 mb-3">
 		<div class="content-block">
 			<h1 class="title mt-3">
-				<strong>
-					Marcas y modelos disponibles
-				</strong>
+				<strong>Consúltanos por modelos disponibles</strong>
 			</h1>
 			<h1 class="title  mt-2">
 				<small>
@@ -15,19 +13,17 @@
 
 		<div class="row row-xs mt-4">
 			<div class="col-12 col-md-6 mb-2">
-				<a href="http://www.audi.com.uy?ref=lestido-renting" target="_blank" class="brand-block hover-effect">
+				<div class="brand-block hover-effect">
 					<div class="logo hover-effect">
 						<img src="@/assets/img/audi-logo.png" alt="" class="img-fluid mr-5" />
-						Audi en Uruguay
 					</div>
 					<img src="@/assets/img/brands-models-1.jpg" alt="" class="img-fluid" />
-				</a>
+				</div>
 			</div>
 			<div class="col-12 col-md-6 mb-2">
 				<a href="http://www.volkswagen.com.uy?ref=lestido-renting" target="_blank" class="brand-block hover-effect">
 					<div class="logo hover-effect">
 						<img src="@/assets/img/volkswagen-logo.png" alt="" class="img-fluid mr-4" />
-						Volkswagen en Uruguay
 					</div>
 					<img src="@/assets/img/brands-models-2.jpg" alt="" class="img-fluid" />
 				</a>
